58ab8194a734495006bc84ae5db0d0a36ebb088e
harshipriya <harshiyarragonda22@gmail.com>
2015-11-20 01:59:20 -0600

    commit

---
+++ source/labass10/student/www/js/services.js
@@ -0,0 +1,11 @@
+angular.module('starter.services', [])
+.service('mainservice', function($q, $http) {
+   
+})
+.service('mainservice', function($q, $http) {
+   
+}) 
+.service('RegisterService',function($q, $http){
+    
+})
+;
